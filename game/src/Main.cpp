b9dea8c90ff6bc7021cd6e3aaacf6537e9f94a93
Diego Pinto da Jornada <diego.p.jornada@gmail.com>
2015-10-09 19:22:54 -0300

    Atualiza projeto Eclipse

---
+++ game/src/Main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cmath>
+
+#include "Game.h"
+#include "PlayState.h"
+#include "MenuState.h"
+#include "PauseState.h"
+
+int main(int argc, char **argv)
+{
+    cgf::Game game(5,30);
+
+	game.init("Bomberman (Versao Alternativa)",480,416,false);
+
+    game.changeState(MenuState::instance());
+
+	while(game.isRunning())
+	{
+		game.handleEvents();
+		game.update();
+		game.draw();
+	}
+
+	// cleanup the engine
+	game.clean();
+
+    return 0;
+}
